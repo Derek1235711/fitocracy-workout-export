8d7efc8429e0cef2e3675ce291c6050588e17243
Hexodus <derek1235711@gmail.com>
2019-12-03 18:34:41 -0800

    Starting on CSV output

--- src/main/java/fitness/thunderwave/fitocracy/exporter/FitocracyApiManager.java
+++ src/main/java/fitness/thunderwave/fitocracy/exporter/FitocracyApiManager.java
@@ -1,12 +1,18 @@
 package fitness.thunderwave.fitocracy.exporter;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 import java.util.TimeZone;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -16,27 +22,35 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import fitness.thunderwave.fitocracy.exporter.dtos.DtoFitoRootGroup;
+import fitness.thunderwave.fitocracy.exporter.dtos.DtoFitoWorkout;
 import fitness.thunderwave.fitocracy.exporter.dtos.DtoFitoWorkoutDay;
+import fitness.thunderwave.fitocracy.exporter.dtos.DtoFitoWorkoutExercise;
+import fitness.thunderwave.fitocracy.exporter.dtos.DtoFitoWorkoutExerciseDetail;
+import fitness.thunderwave.fitocracy.exporter.dtos.DtoFitoWorkoutGroupExercise;
+import fitness.thunderwave.fitocracy.exporter.dtos.DtoFitoWorkoutInput;
+import fitness.thunderwave.fitocracy.exporter.dtos.DtoFitoWorkoutSet;
 
 public class FitocracyApiManager {
 	
     final static private String DATE_FORMAT = "yyyy-MM-dd";
     public final static SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat(DATE_FORMAT);
     
-    public void getWorkouts(String fitocracyId, String sessionId, String fitocracyApiBaseUrl, Date startDate) {
+    public void getWorkouts(String fitocracyId, String sessionId, String fitocracyApiBaseUrl, Date startDate) throws IOException {
     	
 		Date startSearchDate = startDate;
 		Date endSearchDate = getEndofDay(new Date());
 		Date tmpDate = startSearchDate;
 
+		
+		BufferedWriter csvWriter = new BufferedWriter(new FileWriter(fitocracyId + ".csv"));
 
 		// doing one day at time sequentially, to keep the load on Fitocracy serves light
 		while(tmpDate.getTime() <=  endSearchDate.getTime()) {
 
-			makeApiCall(fitocracyId, sessionId, fitocracyApiBaseUrl, tmpDate);
+			makeApiCall(csvWriter, fitocracyId, sessionId, fitocracyApiBaseUrl, tmpDate);
 			
 			tmpDate = addDays(tmpDate, 1);
 			try {
@@ -45,13 +59,15 @@ public class FitocracyApiManager {
 			} catch (InterruptedException e) {
 				System.err.println(e.getMessage());
 			}
-
+			csvWriter.flush();
 		}
+		
+		csvWriter.close();
     	
     }
 	
 	
-	private void makeApiCall(String fitocracyId, String sessionId, String fitocracyApiBaseUrl, Date date)  {
+	private void makeApiCall(BufferedWriter csvWriter, String fitocracyId, String sessionId, String fitocracyApiBaseUrl, Date date)  {
 		
 		try {
 			
@@ -70,35 +86,44 @@ public class FitocracyApiManager {
 																				+ DATE_FORMATTER.format(date) 
 																				+ "/",
 																				HttpMethod.GET, entity, String.class);
-			StringBuilder sb = new StringBuilder();
-			sb.append("\nGetting Data for " + fitocracyId + " for date " + DATE_FORMATTER.format(date));
-			sb.append("\nStatusCode=" + respEntity.getStatusCode());
 			
-			System.out.println(sb.toString());
+			System.out.println("Getting Data for " + fitocracyId + " for date " + DATE_FORMATTER.format(date));
 			
-			ObjectMapper objectMapper = new ObjectMapper();
-			String json = respEntity.getBody();
-			
-			if(json != null && json.length() > 0) {
-				try {
-					DtoFitoWorkoutDay data = objectMapper.readValue(json, DtoFitoWorkoutDay.class);
-					System.out.println(data);
-				} catch (JsonProcessingException e) {
-					System.err.println(e.getMessage());
-				} 
-			}
-			
-			
+			if(respEntity.getStatusCode().is2xxSuccessful()) {
+				
+				
+				ObjectMapper objectMapper = new ObjectMapper();
+				String json = respEntity.getBody();
+				
+				if(json != null && json.length() > 0) {
 
-//			List<TbWorkout> tbWorkouts = workoutManager.convert(vwUser.getUserId(), respEntity.getBody());
-//			sb.append("\nNumber of Workouts=" + tbWorkouts.size());
-//
-//			for (TbWorkout tbWorkout : tbWorkouts) {
-//				sb.append("\n   tbWorkout:" + tbWorkout);
-//				workoutDao.saveTbWorkout(tbWorkout);
-//			}
-//			
-//			logger.debug(sb.toString());
+					boolean foundData = false;
+					
+					try {
+						DtoFitoWorkoutDay data = objectMapper.readValue(json, DtoFitoWorkoutDay.class);
+						if(data.getData() != null && data.getData().size() > 0) {
+							foundData = true;
+							System.out.println("found " + data.getData().size() + " workout(s)");
+						} else {
+							System.out.println("found 0 workout(s)");
+						}
+						
+					} catch (JsonProcessingException e) {
+						System.err.println(e.getMessage());
+					} 
+					
+					if(foundData) {
+						try {
+							BufferedWriter writer = new BufferedWriter(new FileWriter(fitocracyId + "_" + DATE_FORMATTER.format(date) + ".json"));
+							writer.write(json);
+							writer.close();
+						} catch (IOException e1) {
+							e1.printStackTrace();
+						}
+					}
+
+				}
+			}
 			
 		} catch(HttpClientErrorException e) {
 			System.err.println(e.getMessage());
@@ -106,6 +131,194 @@ public class FitocracyApiManager {
 		}
 
 	}
+	
+
+    
+
+    
+	public void processWorkoutDay(BufferedWriter csvWriter, DtoFitoWorkoutDay day) throws Exception {
+		
+		if(day == null) {
+			return;
+		}
+		
+		for (DtoFitoWorkout fitoWorkout : day.getData()) {
+			
+			if(fitoWorkout.getRootGroup() != null) {
+				
+				processWorkout(csvWriter, fitoWorkout, fitoWorkout.getRootGroup());
+				
+				
+				// should always be group for the root
+				if("group".equals(fitoWorkout.getRootGroup().getType())) {
+					
+					List<DtoFitoWorkoutExercise> children = fitoWorkout.getRootGroup().getChildren();
+					
+					int exSeqId = 1;
+					for (DtoFitoWorkoutExercise child : children) {
+						if("group".equals(child.getType())) {
+							
+							processWorkoutGroup(csvWriter, child);
+							
+							processGroupChildrenExercise(csvWriter, child.getChildren());
+							
+						} else if("exercise".equals(child.getType())) {
+							processChildExercise(csvWriter, child, exSeqId);
+							exSeqId += 1;
+						}
+					}
+				} 
+			}
+		}
+	}
+	
+	private void processWorkout(BufferedWriter csvWriter, DtoFitoWorkout fitoWorkout, DtoFitoRootGroup rootGroup) throws IOException {
+		
+		csvWriter.write(toCsv(rootGroup.getName()));
+		csvWriter.write(toCsv(rootGroup.getNotes()));
+		csvWriter.write(toCsv(fitoWorkout.getWorkoutTimestamp().toInstant().toString()));
+
+
+	}
+	
+	private void processWorkoutGroup(BufferedWriter csvWriter, DtoFitoWorkoutExercise group) throws IOException {
+
+		csvWriter.write(toCsv(group.getName()));
+	}
+
+	private void processGroupChildrenExercise(BufferedWriter csvWriter, List<DtoFitoWorkoutGroupExercise> exercises) throws Exception {
+		if(exercises == null) {
+			return;
+		}
+		
+		int exSeq = 1;
+		for (DtoFitoWorkoutGroupExercise ex : exercises) {
+			
+			
+			DtoFitoWorkoutExerciseDetail detail = ex.getExercise();
+			
+			if(detail != null) {
+				processExercise(csvWriter, detail, exSeq);
+				List<DtoFitoWorkoutSet> sets = detail.getSets();
+				
+				int setSeq = 1;
+				for (DtoFitoWorkoutSet set : sets) {
+					
+					processSet(csvWriter, set, setSeq);
+					setSeq++;
+				}
+			}
+			exSeq++;
+			
+		}
+		
+	}
+
+	
+	private void processChildExercise(BufferedWriter csvWriter, DtoFitoWorkoutExercise exercise, int exSeq) throws Exception {
+		if(exercise == null) {
+			return;
+		}
+
+		DtoFitoWorkoutExerciseDetail detail = exercise.getExercise();
+		
+		if(detail != null) {
+			processExercise(csvWriter, detail, exSeq);
+			List<DtoFitoWorkoutSet> sets = detail.getSets();
+			
+			int setSeq = 1;
+			for (DtoFitoWorkoutSet set : sets) {
+				
+				processSet(csvWriter, set, setSeq);
+				setSeq++;
+			}
+		}
+
+	}
+	
+	private void processExercise(BufferedWriter csvWriter, DtoFitoWorkoutExerciseDetail exerciseDetail, int seq) throws Exception {
+
+		csvWriter.write(toCsv("" + seq));
+		csvWriter.write(toCsv("" + exerciseDetail.getExerciseId()));
+
+		
+		// TODO get list of exercises
+		
+//		if(exerciseDetail.getExerciseId() != null) {
+//			Long exId = exerciseDetail.getExerciseId();
+//			if(this.fitocracyDuplicatedExerciseMap.containsKey(exId)) {
+//				exId = this.fitocracyDuplicatedExerciseMap.get(exId);
+//				logger.debug("Found duplicate fitocracy exercise:" + exerciseDetail.getExerciseId() + " -> " + exId);
+//			}
+//			VwExercise vwExercise = viewDao.getVwExerciseByOldId(exId);
+//			if(vwExercise != null) {
+//				tbWorkoutExercise.setExerciseId(vwExercise.getExerciseId());
+//			} else {
+//				throw new Exception("Can't find exercise: " + exerciseDetail);
+//			}
+//		}
+
+		
+	}
+	
+	
+	private void processSet(BufferedWriter csvWriter, DtoFitoWorkoutSet set, int seq) throws IOException {
+
+		csvWriter.write(toCsv("" + seq));
+		
+		if(set.getPoints() != null) {
+			csvWriter.write(toCsv("" + set.getPoints().longValue()));
+		}
+
+		List<DtoFitoWorkoutInput> inputs = set.getInputs();
+		
+		for (DtoFitoWorkoutInput input : inputs) {
+			
+			if("distance".equals(input.getType())) {
+				
+				csvWriter.write(toCsv(input.getUnit()));
+				csvWriter.write(toCsv("" + input.getValue()));
+				
+			} else if("time".equals(input.getType())) {
+				
+				csvWriter.write(toCsv(input.getUnit()));
+				csvWriter.write(toCsv("" + input.getValue()));
+				
+			} else if("reps".equals(input.getType())) {
+				
+				csvWriter.write(toCsv("" + input.getValue()));
+				
+			} else if("weight".equals(input.getType())) {
+				
+				csvWriter.write(toCsv(input.getUnit()));
+				csvWriter.write(toCsv("" + input.getValue()));
+
+			}
+
+			if("assisted".equals(input.getAssistType())) {
+				
+				csvWriter.write(toCsv(input.getAssistType()));
+				csvWriter.write(toCsv(input.getUnit()));
+				csvWriter.write(toCsv("" + input.getValue()));
+				
+			} else if("weighted".equals(input.getAssistType())) { 
+
+				csvWriter.write(toCsv(input.getAssistType()));
+				csvWriter.write(toCsv(input.getUnit()));
+				csvWriter.write(toCsv("" + input.getValue()));
+
+			}
+		}
+	}
+	
+	public String toCsv(String input) {
+		if(StringUtils.isNotEmpty(input)) {
+			return StringEscapeUtils.escapeCsv(input) + ",";
+		} else {
+			return ",";
+		}
+	}
+
 	
     public static Date getEndofDay(Date date) {
         GregorianCalendar cal = new GregorianCalendar(TimeZone.getTimeZone("GMT"));
@@ -117,12 +330,11 @@ public class FitocracyApiManager {
         return cal.getTime();
     }
     
-    public static Date addDays(Date date, int days)
-    {
+    public static Date addDays(Date date, int days) {
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
-        cal.add(Calendar.DATE, days); //minus number would decrement the days
+        cal.add(Calendar.DATE, days);
         return cal.getTime();
     }
-
+	
 }
